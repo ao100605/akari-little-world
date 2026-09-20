77999db62f4ded1f1e1e6583bf7c04206b600449
akari106 <akari106@outlook.com>
2026-09-19 23:44:18 -0700

    Update playground caption

--- sections/Playground.tsx
+++ sections/Playground.tsx
@@ -8,7 +8,7 @@ export default function Playground() {
       <SectionKicker number="03" japanese="遊び場">PLAYGROUND</SectionKicker>
       <div className="section-intro">
         <h2>Some things don&apos;t need a reason.</h2>
-        <p>Little experiments and creative sparks live here.</p>
+        <p>Little experiments and creative sparks live here. Click into them to see multiple entires!</p>
       </div>
       <div className="playground-grid">
         {playgrounds.map((item) => (
